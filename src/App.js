a7923e862a448cadf0b8988686e7f87dbf372cc3
Hajin Lee <betterthanhajin@icloud.com>
2024-04-03 12:52:27 +0900

    feat: add test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import "./App.css";
-import Lottie from "lottie-react";
+import Lottie, { useLottie, useLottieInteractivity } from "lottie-react";
 import sealAnimation from "../src/seals.json";
+import coffeeAnimation from "../src/coffie.json";
+
+const options = {
+  animationData: sealAnimation,
+};
+
+const Example = () => {
+  const lottieObj = useLottie(options);
+  const Animation = useLottieInteractivity({
+    lottieObj,
+    mode: "scroll",
+    actions: [
+      {
+        visibility: [0.4, 0.9],
+        type: "seek",
+        frames: [0, 300],
+      },
+    ],
+  });
+
+  return Animation;
+};
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Lottie animationData={sealAnimation} />{" "}
+        <Example /> <Lottie animationData={coffeeAnimation} />
       </div>
     </div>
   );
